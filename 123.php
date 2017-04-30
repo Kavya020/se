4743895f09ce5ab067ee2f51f248b14ad9bd997d
Kavya020 <kavya.pasupuleti2014@vit.ac.in>
2017-04-30 22:56:21 +0530

    123.php

---
+++ 123.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = " ";
+$dbname = "project";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "UPDATE room SET status='alloted' WHERE room_no=$room_no";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Room alloted ";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
