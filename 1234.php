d703af3d943bcdd1e4459055c53ea5b31f2a3ac6
Kavya020 <kavya.pasupuleti2014@vit.ac.in>
2017-05-02 08:06:43 +0530

    Create 1234.php

---
+++ 1234.php
@@ -0,0 +1,20 @@
+<?php
+
+$link = mysqli_connect("localhost", "root", "", "project");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Attempt update query execution
+$sql = "UPDATE persons SET status='alloted' WHERE room_no=101;
+if(mysqli_query($link, $sql)){
+    echo "Records were updated successfully.";
+} else {
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+?>
